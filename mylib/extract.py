4dc35ca0044e729bb9fac09f3cf52db7f53a2a9e
jamie <liujamie23@gmail.com>
2024-10-06 21:41:14 -0400

    init data

--- mylib/extract.py
+++ mylib/extract.py
@@ -5,13 +5,12 @@ food dataset
 """
 import requests
 
-def extract(url="https://raw.githubusercontent.com/Barabasi-Lab/GroceryDB/main/data/GroceryDB_IgFPro.csv", 
-            file_path="data/GroceryDB_IgFPro.csv"):
+def extract(url="https://raw.githubusercontent.com/fivethirtyeight/data/refs/heads/master/alcohol-consumption/drinks.csv", 
+            file_path="data/drinks.csv"):
     """"Extract a url to a file path"""
     with requests.get(url) as r:
         with open(file_path, 'wb') as f:
             f.write(r.content)
     return file_path
-
 
 
